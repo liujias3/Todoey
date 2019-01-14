6c9f3b5cdd1657173048903113ed6ab719fe523d
liujias3 <liujias3@msu.edu>
2019-01-14 18:29:47 -0500

    persisting data

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,17 +10,18 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray=["Find Mike","Eat egg","Destory Demogorgon"]
+    var itemArray=[Item]()
     
-    let defaults=UserDefaults.standard
-    
+    let dataFilePath=FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items=defaults.array(forKey: "TodoListArray") as? [String]{
-            itemArray=items
-        }
+        loadItems()
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
+//            itemArray = items
+//        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -33,7 +34,9 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item=itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -41,13 +44,11 @@ class TodoListViewController: UITableViewController {
     //Mark - Table
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        self.saveItems()
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -57,10 +58,16 @@ class TodoListViewController: UITableViewController {
         var textField = UITextField()
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            let newItem = Item()
+            newItem.title = textField.text!
+            
             //what will happen item pressed
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray,forKey: "TodoListArray")
-            self.tableView.reloadData()
+            self.itemArray.append(newItem)
+            
+           
+            self.saveItems()
+            
+            
         }
         alert.addTextField{(alertTextField) in
             alertTextField.placeholder = "Create new Item"
@@ -71,6 +78,28 @@ class TodoListViewController: UITableViewController {
         
         present(alert, animated: true, completion: nil)
         
+    }
+    
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }catch{
+            print("Error encoding")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(){
+        if let data=try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+                print("error decoding item array")
+            }
+        }
     }
 }
 
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by jiashun liu on 1/14/19.
+//  Copyright © 2019 jiashun liu. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable{
+    
+    var title : String = ""
+    var done : Bool = false
+}
+
